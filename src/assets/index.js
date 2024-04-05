b691ed84b52dc3a21ecc416b0e5a9565eafd4d7e
Sahil Khan <sahilkhan0471@gmail.com>
2024-04-05 10:51:01 +0530

    project initalized

---
+++ src/assets/index.js
@@ -0,0 +1,26 @@
+import Add from "./add.svg";
+import AddImage from "./addImage.svg";
+import ArrowDown from "./arrowDown.svg";
+import ArrowUp from "./arrowUp.svg";
+import Help from "./help.svg";
+import History from "./history.svg";
+import Menu from "./menu.svg";
+import Message from "./message.svg";
+import Mic from "./mic.svg";
+import MoreVertical from "moreVertical.svg";
+import Settings from "./settings.svg";
+
+
+export {
+    Add,
+    AddImage,
+    ArrowDown,
+    ArrowUp,
+    Help,
+    History,
+    Menu,
+    Message,
+    Mic,
+    MoreVertical,
+    Settings
+};
